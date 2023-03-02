17184ad4387ad84274ebc180e51fe1f5c850440a
zhangrunping <1204495626@qq.com>
2023-03-02 12:16:30 +0800

    初始化

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,20 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+npm run build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+# cd public
 
-git init
-git add -A
+# git init
+git add .
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
+# git remote add origin https://github.com/sanyuan0704/my_blog.git
 
-git push -f git@github.com:Zhangrunping/blogs.git master:gh-pages
+git push -f origin  master:master
 
-cd -
+cd ..
+
+tcb hosting:deploy public -e blog-9g8lgnuke4603ff9
